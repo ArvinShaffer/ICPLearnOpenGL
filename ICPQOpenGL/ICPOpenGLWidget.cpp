ec2b9563974ea9b9d67a6515e8ee29b9e0618abd
xsr <icuipi@gmail.com>
2025-02-13 17:15:27 +0800

    paint triangles

--- ICPQOpenGL/ICPOpenGLWidget.cpp
+++ ICPQOpenGL/ICPOpenGLWidget.cpp
@@ -1,5 +1,12 @@
 #include "ICPOpenGLWidget.h"
 
+unsigned int VAO, VBO;
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
 ICPOpenGLWidget::ICPOpenGLWidget(QWidget *parent)
     : QOpenGLWidget{parent}
 {}
@@ -7,6 +14,16 @@ ICPOpenGLWidget::ICPOpenGLWidget(QWidget *parent)
 void ICPOpenGLWidget::initializeGL()
 {
     this->initializeOpenGLFunctions();
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void ICPOpenGLWidget::resizeGL(int w, int h)
@@ -18,4 +35,7 @@ void ICPOpenGLWidget::paintGL()
 {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
 }
