72ff87c005e9897c8273d44d6aedebcb9fe2b466
xsr <icuipi@gmail.com>
2025-02-17 11:16:29 +0800

    add vertex shader and fragment shader

--- ICPQOpenGL/ICPOpenGLWidget.cpp
+++ ICPQOpenGL/ICPOpenGLWidget.cpp
@@ -1,11 +1,24 @@
 #include "ICPOpenGLWidget.h"
 
 unsigned int VAO, VBO;
+unsigned int shaderProgram;
 float vertices[] = {
     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
 };
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" "}\n\0";
 
 ICPOpenGLWidget::ICPOpenGLWidget(QWidget *parent)
     : QOpenGLWidget{parent}
@@ -24,6 +37,23 @@ void ICPOpenGLWidget::initializeGL()
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // 编译顶点着色器
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // 编译片段着色器
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // link shaders
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
 }
 
 void ICPOpenGLWidget::resizeGL(int w, int h)
