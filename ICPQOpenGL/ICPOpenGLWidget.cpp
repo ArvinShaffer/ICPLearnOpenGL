4200a2e9125624b047e08fb2faef101940617299
xsr <icuipi@gmail.com>
2025-02-19 13:57:04 +0800

    EBO

--- ICPQOpenGL/ICPOpenGLWidget.cpp
+++ ICPQOpenGL/ICPOpenGLWidget.cpp
@@ -1,11 +1,17 @@
 #include "ICPOpenGLWidget.h"
 
-unsigned int VAO, VBO;
+unsigned int VAO, VBO, EBO;
 unsigned int shaderProgram;
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f
+};
+
+unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
 };
 
 const char *vertexShaderSource = "#version 330 core\n"
@@ -35,8 +41,13 @@ void ICPOpenGLWidget::initializeGL()
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (GLvoid*)0);
     glEnableVertexAttribArray(0);
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //glBindVertexArray(0);
+    //glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // EBO
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // 编译顶点着色器
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -54,6 +65,11 @@ void ICPOpenGLWidget::initializeGL()
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void ICPOpenGLWidget::resizeGL(int w, int h)
@@ -66,6 +82,7 @@ void ICPOpenGLWidget::paintGL()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0/*&indices*/);
     glBindVertexArray(0);
 }
